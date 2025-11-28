57019253fe5418d359ed6b060ff934530b77c2f3
MARK PARSON <anon4me785@gmail.com>
2025-11-28 11:00:23 -0700

    Add Leaflet CSS link to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+      </head>
       <body>{children}</body>
     </html>
   );
